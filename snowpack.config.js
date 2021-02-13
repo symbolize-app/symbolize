fe8b8ff79d3297312950ffa0c2a8158d0e3a4174
Dan Cecile <dancecile@gmail.com>
2021-02-13 16:48:42 -0500

    Enable Snowpack minify 🔧

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,23 +1,16 @@
-// Snowpack Configuration File
-// See all supported options: https://www.snowpack.dev/reference/configuration
-
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
     public: { url: '/', static: true },
     ui: { url: '/js/ui' },
-    /* ... */
   },
-  plugins: [
-    /* ... */
-  ],
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
+  plugins: [],
+  packageOptions: {},
+  devOptions: {},
+  buildOptions: {},
+  optimize: {
+    bundle: false,
+    minify: true,
+    target: 'es2018',
   },
 }
