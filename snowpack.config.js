be063668d2bfd145245efc654040652b04967743
Dan Cecile <dancecile@gmail.com>
2021-02-13 16:37:20 -0500

    Initial commit 🌱

---
+++ snowpack.config.js
@@ -0,0 +1,23 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: { url: '/', static: true },
+    ui: { url: '/js/ui' },
+    /* ... */
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+}
