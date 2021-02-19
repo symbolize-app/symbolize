69fb00bb09b5cc5e18b07dc9f666caffe0aab7f7
Dan Cecile <dancecile@gmail.com>
2021-02-18 23:55:08 -0500

    Set up all widget types ✨

--- ui/index.ts
+++ ui/index.ts
@@ -1,8 +1,106 @@
-import { initStyles, renderStyle } from '@tiny/ui/style'
+import { initStyles } from '@tiny/ui/style'
+import { renderStyle } from '@tiny/ui/style'
+import { Widget } from '@tiny/ui/widget'
+import { html } from '@tiny/ui/widget'
+import { widget } from '@tiny/ui/widget'
+import { HtmlListeners } from '@tiny/ui/widget'
+import { range } from '@tiny/ui/widget'
+import { replaceHtml } from '@tiny/ui/widget'
 import { message } from '@fe/ui/message'
+
 console.log(message)
 initStyles()
 const s = renderStyle({
   color: 'red',
 })
 console.log('CLASSES', s)
+
+const myButton = widget<{
+  readonly body: Widget
+  listen: HtmlListeners
+}>(() => {
+  const body = html.button({
+    content: ['OK'],
+  })
+  return {
+    body,
+    set listen(value: HtmlListeners) {
+      body.listen = value
+    },
+  }
+})
+
+const myCounter = widget<{
+  readonly body: Widget
+  value: number
+}>(() => {
+  let value: number
+  const body = html.span({})
+  const result = {
+    body,
+    get value() {
+      return value
+    },
+    set value(_value) {
+      value = _value
+      body.content = [value.toString()]
+    },
+  }
+  result.value = 0
+  return result
+})
+
+const counter = myCounter({})
+
+const listContents = range({
+  content: [
+    html.li({
+      content: ['init'],
+    }),
+  ],
+})
+
+const div = html.div({
+  content: [
+    'HI ',
+    myButton({
+      listen: {
+        click(event) {
+          console.log(event)
+          counter.value += 1
+          listContents.content = [
+            html.li({
+              content: [counter.value.toString()],
+            }),
+            html.li({
+              content: [(counter.value * 2).toString()],
+            }),
+          ]
+        },
+      },
+    }),
+    ' ',
+    counter,
+    html.ul({
+      content: [
+        html.li({
+          content: ['+'],
+        }),
+        listContents,
+        html.li({
+          content: ['-'],
+        }),
+      ],
+    }),
+  ],
+})
+
+replaceHtml(
+  html.html({
+    content: [
+      html.body({
+        content: [div],
+      }),
+    ],
+  })
+)
