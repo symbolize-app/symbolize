d27d861b25a60d143d92d4aefa3da74c58bc364a
Dan Cecile <dancecile@gmail.com>
2021-03-06 15:31:50 -0500

    Set up node build ✨

--- api/index.t.ts
+++ api/index.t.ts
@@ -1,40 +1,22 @@
 import * as test from '@tiny/test/index.ts'
-import jsdom from 'jsdom'
+import perfHooks from 'perf_hooks'
 import url from 'url'
-import vm from 'vm'
 
 export const all: test.TestCollection = () => []
 
-export async function run(): Promise<void> {
-  const dom = new jsdom.JSDOM('', {
-    runScripts: 'outside-only',
-  })
-  const script = new vm.Script(`
-    if (!this.ran) {
-      this.ran = 0;
-    }
-
-    ++this.ran;
-  `)
-
-  const vmContext = dom.getInternalVMContext()
-
-  script.runInContext(vmContext)
-  script.runInContext(vmContext)
-  script.runInContext(vmContext)
-
-  console.assert(dom.window.ran === 4)
-
+export async function run(): Promise<boolean> {
+  const ctx = {
+    now: () => perfHooks.performance.now(),
+  }
   const coreTest = await import('@fe/core/index.t.ts')
   const apiTest = await import('@fe/api/index.t.ts')
-  const uiTest = await import('@fe/ui/index.t.ts')
-  await test.runAll([coreTest, apiTest, uiTest])
+  return await test.runAll(ctx, [coreTest, apiTest])
 }
 
 if (
   process.argv[1] === url.fileURLToPath(import.meta.url)
 ) {
-  run().catch((error) => {
-    throw error
+  void run().then((success) => {
+    process.exit(success ? 0 : 1)
   })
 }
