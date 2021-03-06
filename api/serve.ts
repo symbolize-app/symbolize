58171334be1e65d65e2ce8bcd80194a93969bdc5
Dan Cecile <dancecile@gmail.com>
2021-03-06 15:31:50 -0500

    Set up node build ✨

--- api/serve.ts
+++ api/serve.ts
@@ -1,5 +1,6 @@
 import * as message from '@fe/core/message.ts'
 import * as http from 'http'
+import urlModule from 'url'
 
 export function handleRequest(
   req: http.IncomingMessage,
@@ -9,4 +10,16 @@ export function handleRequest(
   console.log('GOT', req.url)
   res.write(message.hi)
   res.end()
+}
+
+function main() {
+  const server = http.createServer(handleRequest)
+  server.listen(8001)
+}
+
+if (
+  process.argv[1] ===
+  urlModule.fileURLToPath(import.meta.url)
+) {
+  void main()
 }
