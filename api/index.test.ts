3aadf43052de2e3843c293421728ff514cb3f935
Dan Cecile <dancecile@gmail.com>
2021-03-10 22:32:42 -0500

    Set up jsdom tests 🔨

--- api/index.test.ts
+++ api/index.test.ts
@@ -1,13 +1,18 @@
 import * as test from '@tiny/test/index.ts'
+import * as widget from '@tiny/ui/widget.ts'
+import jsdom from 'jsdom'
 import * as perfHooks from 'perf_hooks'
 
 export const all: test.TestCollection = () => []
 
 export async function run(): Promise<boolean> {
+  const dom = new jsdom.JSDOM()
   const ctx = {
+    ...widget.initContext(dom.window.document),
     now: () => perfHooks.performance.now(),
   }
   const coreTest = await import('@fe/core/index.test.ts')
   const apiTest = await import('@fe/api/index.test.ts')
-  return await test.runAll(ctx, [coreTest, apiTest])
+  const uiTest = await import('@fe/ui/index.test.ts')
+  return await test.runAll(ctx, [coreTest, apiTest, uiTest])
 }
--- ui/button.test.ts
+++ ui/button.test.ts
@@ -1,16 +1,12 @@
 import * as button from '@fe/ui/button.ts'
 import * as widgetTest from '@tiny/ui/widget.test.ts'
-import * as widget from '@tiny/ui/widget.ts'
+import type * as widget from '@tiny/ui/widget.ts'
 import chai from 'chai'
 
 export const url = import.meta.url
 
 export const tests = {
-  ['button text'](): void {
-    const document = window.document.implementation.createHTMLDocument(
-      ''
-    )
-    const ctx = widget.initContext(document)
+  ['button text'](ctx: widget.WidgetContext): void {
     const buttonElement = widgetTest.collectOne(
       button.custom(ctx, {})
     )
