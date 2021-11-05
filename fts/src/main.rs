366580df7f1bca2e3c59f9c491c74086fccbe475
Dan Cecile <dancecile@gmail.com>
2021-11-04 23:47:41 -0400

    Use Tower service ♻️

--- fts/src/main.rs
+++ fts/src/main.rs
@@ -1,20 +1,21 @@
 mod message;
 
 use crate::message::get_message;
-use hyper::service::make_service_fn;
-use hyper::service::service_fn;
+use hyper::service::Service;
 use hyper::Body;
 use hyper::Request;
 use hyper::Response;
 use hyper::Server;
 use std::collections::HashMap;
-use std::convert::Infallible;
 use std::env;
 use std::error::Error;
 use std::fs::create_dir_all;
+use std::future::Future;
 use std::net::Ipv4Addr;
 use std::net::SocketAddr;
 use std::path::Path;
+use tower::make::Shared;
+use std::pin::Pin;
 use tantivy::collector::TopDocs;
 use tantivy::directory::MmapDirectory;
 use tantivy::doc;
@@ -28,11 +29,74 @@ use tantivy::IndexReader;
 use tantivy::ReloadPolicy;
 use url::form_urlencoded;
 
+#[derive(Clone)]
+struct Context {
+  schema: Schema,
+  index: Index,
+  index_reader: IndexReader,
+  fields: FieldSet,
+}
+
+#[derive(Clone)]
+struct FieldSet {
+  title: Field,
+  body: Field,
+}
+
+impl Service<Request<Body>> for Context {
+  type Response = Response<Body>;
+  type Error = Box<dyn Error + Send + Sync>;
+
+  #[allow(clippy::type_complexity)]
+  type Future = Pin<
+    Box<
+      dyn Future<Output = Result<Self::Response, Self::Error>>
+        + Send
+        + Sync,
+    >,
+  >;
+
+  fn poll_ready(
+    &mut self,
+    _cx: &mut std::task::Context<'_>,
+  ) -> std::task::Poll<Result<(), Self::Error>> {
+    std::task::Poll::Ready(Ok(()))
+  }
+
+  fn call(&mut self, req: Request<Body>) -> Self::Future {
+    let context = self.clone();
+    Box::pin(hello_world(context, req))
+  }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
   let host: Ipv4Addr = env::var("FTS_HOST")?.parse()?;
   let port: u16 = env::var("FTS_PORT")?.parse()?;
   let addr = SocketAddr::from((host, port));
+
+  let context = load_context()?;
+  let server =
+    Server::bind(&addr).serve(Shared::new(context));
+  server.await?;
+
+  Ok(())
+}
+
+fn load_context() -> Result<Context, Box<dyn Error>> {
+  let (schema, index, index_reader, fields) = load_index()?;
+  Ok(Context {
+    schema,
+    index,
+    index_reader,
+    fields,
+  })
+}
+
+fn load_index() -> Result<
+  (Schema, Index, IndexReader, FieldSet),
+  Box<dyn Error>,
+> {
   let index_path = Path::new("./.tantivy/topic");
   create_dir_all(index_path)?;
   let index_dir = MmapDirectory::open(index_path)?;
@@ -82,70 +146,46 @@ async fn main() -> Result<(), Box<dyn Error>> {
     prepared_commit.commit()?;
   }
 
-  let reader = index
+  let index_reader = index
     .reader_builder()
     .reload_policy(ReloadPolicy::OnCommit)
     .try_into()?;
 
-  let make_service = make_service_fn(move |_| {
-    let schema = schema.clone();
-    let index = index.clone();
-    let reader = reader.clone();
-    // https://github.com/hyperium/hyper/blob/ed2fdb7b6a2963cea7577df05ddc41c56fee7246/examples/state.rs
-    async move {
-      Ok::<_, Infallible>(service_fn(
-        move |req: Request<Body>| {
-          hello_world(
-            schema.clone(),
-            title,
-            body,
-            index.clone(),
-            reader.clone(),
-            req,
-          )
-        },
-      ))
-    }
-  });
-
-  let server = Server::bind(&addr).serve(make_service);
-
-  server.await?;
-
-  Ok(())
+  Ok((
+    schema,
+    index,
+    index_reader,
+    FieldSet { title, body },
+  ))
 }
 
 #[allow(clippy::unused_async)]
 async fn hello_world(
-  schema: Schema,
-  title: Field,
-  body: Field,
-  index: Index,
-  reader: IndexReader,
+  context: Context,
   req: Request<Body>,
-) -> Result<Response<Body>, Infallible> {
-  let searcher = reader.searcher();
-  let query_parser =
-    QueryParser::for_index(&index, vec![title, body]);
+) -> Result<Response<Body>, Box<dyn Error + Send + Sync>> {
+  let searcher = context.index_reader.searcher();
+  let query_parser = QueryParser::for_index(
+    &context.index,
+    vec![context.fields.title, context.fields.body],
+  );
   print!("{}", get_message());
-  let query = query_parser
-    .parse_query(
-      form_urlencoded::parse(
-        req.uri().query().unwrap().as_bytes(),
-      )
-      .into_owned()
-      .collect::<HashMap<String, String>>()
-      .get("q")
-      .unwrap(),
+  let query = query_parser.parse_query(
+    form_urlencoded::parse(
+      req.uri().query().ok_or("invalid query")?.as_bytes(),
     )
-    .unwrap();
-  let top_docs = searcher
-    .search(&query, &TopDocs::with_limit(10))
-    .unwrap();
+    .into_owned()
+    .collect::<HashMap<String, String>>()
+    .get("q")
+    .ok_or("missing query param")?,
+  )?;
+  let top_docs =
+    searcher.search(&query, &TopDocs::with_limit(10))?;
   let mut result = "".to_string();
   for (_score, doc_address) in top_docs {
-    let retrieved_doc = searcher.doc(doc_address).unwrap();
-    result.push_str(&schema.to_json(&retrieved_doc));
+    let retrieved_doc = searcher.doc(doc_address)?;
+    result
+      .push_str(&context.schema.to_json(&retrieved_doc));
   }
   Ok(Response::new(result.into()))
 }
