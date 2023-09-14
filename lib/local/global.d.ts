93d6262714ac37e79830515f030fb1580b270dd1
Dan Cecile <dancecile@gmail.com>
2023-09-13 22:12:51 -0400

    Create remaining packages

--- lib/local/global.d.ts
+++ lib/local/global.d.ts
@@ -1,28 +1,5 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
-declare module '*.sql' {
-  const text: string
-  // eslint-disable-next-line import/no-default-export
-  export default text
-}
-
-declare module 'stream' {
-  export namespace Readable {
-    function toWeb(
-      streamReadable: NodeJS.ReadableStream
-    ): ReadableStream
-    function fromWeb(
-      readableStream: ReadableStream,
-      options?: {
-        encoding: string
-        highWaterMark: number
-        objectMode: boolean
-        signal: AbortSignal
-      }
-    ): NodeJS.ReadableStream
-  }
-}
-
 declare module 'node-fetch/src/body.js' {
   import type * as nodeBuffer from 'node:buffer'
   import type * as nodeFetch from 'node-fetch'
@@ -43,11 +20,5 @@ declare module 'node-fetch/src/body.js' {
     blob(): Promise<nodeBuffer.Blob>
     json(): Promise<unknown>
     text(): Promise<string>
-  }
-}
-
-interface ImportMeta {
-  env: {
-    [key: string]: string
   }
 }
