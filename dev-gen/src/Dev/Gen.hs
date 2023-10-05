66059122465ac6edd30d148657d79032433237bb
Dan Cecile <dancecile@gmail.com>
2023-10-05 01:06:52 -0400

    Write YAML and JSON files

--- dev-gen/src/Dev/Gen.hs
+++ dev-gen/src/Dev/Gen.hs
@@ -5,6 +5,7 @@ module Dev.Gen
 where
 
 import Data.Aeson qualified as Aeson
+import Data.Aeson.KeyMap qualified as Aeson.KeyMap
 import Data.Vector qualified as Vector
 import Dev.Gen.Exec qualified as Exec
 import Dev.Gen.FileFormat qualified as FileFormat
@@ -19,4 +20,9 @@ test' :: Exec.Exec Aeson.Value
 test' = do
   a <- Exec.readFile "../pnpm-workspace.yaml" FileFormat.YAML
   b <- Exec.readFile "../package.json" FileFormat.YAML
+  Exec.writeFile "../tmp/x.yaml" FileFormat.YAML
+    . Aeson.Object
+    $ Aeson.KeyMap.fromList
+      [ ("key", Aeson.String "value")
+      ]
   pure . Aeson.Array . Vector.fromList $ [a, b]
--- dev-gen/src/Dev/Gen/Interpret.hs
+++ dev-gen/src/Dev/Gen/Interpret.hs
@@ -8,9 +8,9 @@ import Data.Yaml qualified as Yaml
 import Dev.Gen.Command qualified as Command
 import Dev.Gen.Exec qualified as Exec
 import Dev.Gen.FileFormat qualified as FileFormat
-import Relude.Applicative (Applicative (pure), pass)
-import Relude.File (readFileBS, readFileLBS)
-import Relude.Function ((.))
+import Relude.Applicative (Applicative (pure))
+import Relude.File (readFileBS, readFileLBS, writeFileBS, writeFileLBS)
+import Relude.Function (($), (.))
 import Relude.Monad (Either (Left, Right), Monad ((>>=)), MonadFail (fail), MonadIO (liftIO))
 import Relude.String (show)
 
@@ -29,5 +29,7 @@ interpret (Exec.Command (Command.ReadFile filePath FileFormat.JSON)) = do
   case Aeson.eitherDecode bytes of
     (Left error) -> liftIO (fail error)
     (Right value) -> pure value
-interpret (Exec.Command (Command.WriteFile {})) =
-  pass
+interpret (Exec.Command (Command.WriteFile filePath FileFormat.YAML value)) =
+  writeFileBS filePath $ Yaml.encode value
+interpret (Exec.Command (Command.WriteFile filePath FileFormat.JSON value)) =
+  writeFileLBS filePath $ Aeson.encode value
