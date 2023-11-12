bdc27dc33a15ff6bca708b2a22bce511263041cf
Dan Cecile <dancecile@gmail.com>
2023-11-11 23:37:33 -0500

    Remove unused packages

--- lib-crypto/crypto.node.ts
+++
@@ -1,9 +0,0 @@
-import sodiumNative from 'sodium-native'
-
-export function hash(input: Buffer): Buffer {
-  const output = Buffer.alloc(
-    sodiumNative.crypto_generichash_BYTES
-  )
-  sodiumNative.crypto_generichash(output, input)
-  return output
-}
