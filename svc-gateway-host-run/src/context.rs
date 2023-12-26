6100b7ea26bb87dd97fa7cb34b2001c56c802c82
Dan Cecile <dancecile@gmail.com>
2023-12-26 10:43:56 -0500

    Introduce generic context trait

--- svc-gateway-host-run/src/context.rs
+++ svc-gateway-host-run/src/context.rs
@@ -1,8 +1,34 @@
 use crate::db as svc_db;
-use std::sync::Arc;
-use std::sync::Mutex;
+#[cfg(test)]
+use mockall::mock;
 
 #[derive(Clone)]
-pub struct Context {
-  pub db: Arc<Mutex<svc_db::Context>>,
+pub struct MainContext {
+  pub db: svc_db::MainContext,
+}
+
+pub trait DbContext
+where
+  Self::Inner: svc_db::Context,
+{
+  type Inner;
+
+  fn db(&self) -> &Self::Inner;
+}
+
+#[cfg(test)]
+mock! {
+  pub DbContext {}
+  impl DbContext for DbContext {
+      type Inner = svc_db::MockContext;
+      fn db(&self) -> &<Self as DbContext>::Inner;
+  }
+}
+
+impl DbContext for MainContext {
+  type Inner = svc_db::MainContext;
+
+  fn db(&self) -> &Self::Inner {
+    &self.db
+  }
 }
