641bf9d68376690956836e86c8d1262e3077ba50
Dan Cecile <dancecile@gmail.com>
2024-01-06 00:17:54 -0500

    Handle restarting watched processes

---
+++ dev-watchman-client/src/nix_child.rs
@@ -0,0 +1,19 @@
+use anyhow::anyhow;
+use anyhow::Result;
+use nix::sys::signal;
+use nix::sys::signal::Signal;
+use nix::unistd::Pid;
+use tokio::process::Child;
+
+pub trait NixChild {
+  fn signal_kill(&self, signal: Signal) -> Result<()>;
+}
+
+impl NixChild for Child {
+  fn signal_kill(&self, signal: Signal) -> Result<()> {
+    let pid =
+      Pid::from_raw(self.id().ok_or(anyhow!("missing PID"))?.try_into()?);
+    signal::kill(pid, signal)?;
+    Ok(())
+  }
+}
