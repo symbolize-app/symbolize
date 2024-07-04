dcecile/intertwine

Set up some typography APIs for contrast

lib-contrast/prop/index.test.ts
@@ -1,0 +1,21 @@
+import type * as contrast from '@/index.ts'
+import type * as compute from '@intertwine/lib-compute'
+import type * as test from '@intertwine/lib-test'
+
+export const all: test.TestCollection<
+  compute.Context & contrast.Context
+> = () => [
+  import('@/prop/background.test.ts'),
+  import('@/prop/border.test.ts'),
+  import('@/prop/boxSizing.test.ts'),
+  import('@/prop/content.test.ts'),
+  import('@/prop/font.test.ts'),
+  import('@/prop/inset.test.ts'),
+  import('@/prop/isolation.test.ts'),
+  import('@/prop/margin.test.ts'),
+  import('@/prop/overflow.test.ts'),
+  import('@/prop/padding.test.ts'),
+  import('@/prop/pointer.test.ts'),
+  import('@/prop/position.test.ts'),
+  import('@/prop/size.test.ts'),
+]

lib-contrast/prop/index.ts
@@ -1,0 +1,14 @@
+export { background } from '@/prop/background.ts'
+export { border } from '@/prop/border.ts'
+export { boxSizing } from '@/prop/boxSizing.ts'
+export { content } from '@/prop/content.ts'
+export { fill } from '@/prop/fill.ts'
+export { font } from '@/prop/font.ts'
+export { inset } from '@/prop/inset.ts'
+export { isolation } from '@/prop/isolation.ts'
+export { margin } from '@/prop/margin.ts'
+export { overflow } from '@/prop/overflow.ts'
+export { position } from '@/prop/position.ts'
+export { padding } from '@/prop/padding.ts'
+export { pointer } from '@/prop/pointer.ts'
+export { size } from '@/prop/size.ts'

symbolize-app/symbolize

lib-contrast/prop/index.test.ts
@@ -1,0 +1,26 @@
+import type * as contrast from '@/index.ts'
+import type * as compute from '@intertwine/lib-compute'
+import type * as test from '@intertwine/lib-test'
+
+export const all: test.TestCollection<
+  compute.Context & contrast.Context
+> = () => [
+  import('@/prop/accent.test.ts'),
+  import('@/prop/background.test.ts'),
+  import('@/prop/border.test.ts'),
+  import('@/prop/caret.test.ts'),
+  import('@/prop/content.test.ts'),
+  import('@/prop/font.test.ts'),
+  import('@/prop/hyphen.test.ts'),
+  import('@/prop/inset.test.ts'),
+  import('@/prop/isolation.test.ts'),
+  import('@/prop/line.test.ts'),
+  import('@/prop/margin.test.ts'),
+  import('@/prop/overflow.test.ts'),
+  import('@/prop/padding.test.ts'),
+  import('@/prop/pointer.test.ts'),
+  import('@/prop/position.test.ts'),
+  import('@/prop/punctuation.test.ts'),
+  import('@/prop/size.test.ts'),
+  import('@/prop/text.test.ts'),
+]

lib-contrast/prop/index.ts
@@ -1,0 +1,19 @@
+export { accent } from '@/prop/accent.ts'
+export { background } from '@/prop/background.ts'
+export { border } from '@/prop/border.ts'
+export { caret } from '@/prop/caret.ts'
+export { content } from '@/prop/content.ts'
+export { fill } from '@/prop/fill.ts'
+export { font } from '@/prop/font.ts'
+export { hyphen } from '@/prop/hyphen.ts'
+export { inset } from '@/prop/inset.ts'
+export { isolation } from '@/prop/isolation.ts'
+export { line } from '@/prop/line.ts'
+export { margin } from '@/prop/margin.ts'
+export { overflow } from '@/prop/overflow.ts'
+export { position } from '@/prop/position.ts'
+export { padding } from '@/prop/padding.ts'
+export { pointer } from '@/prop/pointer.ts'
+export { punctuation } from '@/prop/punctuation.ts'
+export { size } from '@/prop/size.ts'
+export { text } from '@/prop/text.ts'